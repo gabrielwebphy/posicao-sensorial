9c3263fdb4342c68e25b68d4852386c491544a4d
gabrielwebphy <gabriel.morgado@webphy.systems>
2023-05-23 13:22:53 -0300

    Corrigindo erros

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const yCoord = document.getElementById("ycoord");
 const zCoord = document.getElementById("zcoord");
 const myCanvas = document.getElementById("myCanvas");
 const ctx = myCanvas.getContext("2d");
-const button = document.getElementById('ar-button')
+/*const button = document.getElementById('ar-button')
 button.addEventListener('click', activateXR)
 async function activateXR() {
   // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
@@ -106,4 +106,104 @@ async function activateXR() {
     }
   };
   session.requestAnimationFrame(onXRFrame);
+}*/
+
+let xrButton = document.getElementById("ar-button");
+let xrSession = null;
+let xrRefSpace = null;
+
+// WebGL scene globals.
+let gl = null;
+
+function checkSupportedState() {
+  navigator.xr.isSessionSupported("immersive-ar").then((supported) => {
+    if (supported) {
+      xrButton.innerHTML = "Enter AR";
+    } else {
+      xrButton.innerHTML = "AR not found";
+    }
+
+    xrButton.disabled = !supported;
+  });
 }
+
+function initXR() {
+  if (navigator.xr) {
+    xrButton.addEventListener("click", onButtonClicked);
+    navigator.xr.addEventListener("devicechange", checkSupportedState);
+    checkSupportedState();
+  }
+}
+
+function onButtonClicked() {
+  if (!xrSession) {
+    // Ask for an optional DOM Overlay, see https://immersive-web.github.io/dom-overlays/
+    navigator.xr
+      .requestSession("immersive-ar", {
+        optionalFeatures: ["dom-overlay"],
+        domOverlay: { root: document.getElementById("overlay") },
+      })
+      .then(onSessionStarted, onRequestSessionError);
+  } else {
+    xrSession.end();
+  }
+}
+
+function onSessionStarted(session) {
+  xrSession = session;
+  xrButton.innerHTML = "Exit AR";
+
+  session.addEventListener("end", onSessionEnded);
+  let canvas = document.createElement("canvas");
+  gl = canvas.getContext("webgl", {
+    xrCompatible: true,
+  });
+  session.updateRenderState({ baseLayer: new XRWebGLLayer(session, gl) });
+  session.requestReferenceSpace("local").then((refSpace) => {
+    xrRefSpace = refSpace;
+    session.requestAnimationFrame(onXRFrame);
+  });
+}
+
+function onRequestSessionError(ex) {
+  alert("Failed to start immersive AR session.");
+  console.error(ex.message);
+}
+
+function onEndSession(session) {
+  session.end();
+}
+
+function onSessionEnded(event) {
+  xrSession = null;
+  xrButton.innerHTML = "Enter AR";
+  gl = null;
+}
+
+function onXRFrame(t, frame) {
+  let session = frame.session;
+  session.requestAnimationFrame(onXRFrame);
+
+  gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer.framebuffer);
+
+  // Update the clear color so that we can observe the color in the
+  // headset changing over time. Use a scissor rectangle to keep the AR
+  // scene visible.
+  const width = session.renderState.baseLayer.framebufferWidth;
+  const height = session.renderState.baseLayer.framebufferHeight;
+  gl.enable(gl.SCISSOR_TEST);
+  gl.scissor(width / 4, height / 4, width / 2, height / 2);
+  let time = Date.now();
+  gl.clearColor(Math.cos(time / 2000), Math.cos(time / 4000), Math.cos(time / 6000), 0.5);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  let pose = frame.getViewerPose(xrRefSpace);
+  if (pose) {
+    const p = pose.transform.position;
+    document.getElementById("pose").innerText = "Position: " + p.x.toFixed(3) + ", " + p.y.toFixed(3) + ", " + p.z.toFixed(3);
+  } else {
+    document.getElementById("pose").innerText = "Position: (null pose)";
+  }
+}
+
+initXR();
